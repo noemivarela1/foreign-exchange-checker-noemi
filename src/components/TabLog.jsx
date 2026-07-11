b55ad29167fb755dd6c095b2d967b5f09702d3f3
Noemi <noemivarela1@gmail.com>
2026-07-11 14:29:57 +0200

    tab log made responsive

--- src/components/TabLog.jsx
+++ src/components/TabLog.jsx
@@ -68,13 +68,13 @@ export function TabLog({ amount }) {
 
     // Cando hai internet, hai amount e hai conversións rexistradas
     return (
-        <div className="flex flex-col w-[1036px] gap-250 p-250 text-neutral-200 mx-auto bg-neutral-900/30 rounded-xl border border-neutral-800">
+        <div className="flex flex-col w-[343px] md:w-[720px] xl:w-[1036px] md:gap-250 md:p-250 text-neutral-200 mx-auto bg-neutral-900/30 rounded-xl border border-neutral-800">
             {/* Cabeceira co contador e o botón de borrar */}
-            <div className="flex justify-between items-center border-b border-neutral-800 pb-250">
-                <h3 className="text-preset-3 uppercase text-lime-500 font-bold tracking-wider">
+            <div className="flex flex-col md:flex-row gap-125 justify-between items-center border-b border-neutral-600">
+                <h3 className="text-preset-3 self-start uppercase text-lime-50 tracking-wider">
                     Conversion Log
                 </h3>
-                <div className="flex justify-center items-center gap-200">
+                <div className="flex w-full md:w-1/2 md:justify-end justify-between items-center  gap-200">
                     <p className="uppercase text-preset-5 text-neutral-50 opacity-70">{conversionLog.length} logged</p>
                     <button
                         onClick={clearLog}
@@ -86,28 +86,36 @@ export function TabLog({ amount }) {
             </div>
 
             {/* Lista con scroll se acumulas moitos logs */}
-            <div className="flex flex-col gap-2 max-h-[596px] w-[996px] overflow-y-auto pr-2 no-scrollbar">
+            <div className="flex flex-col gap-2 md:max-h-[596px] gap-250 xl:w-[996px] overflow-y-auto pr-2 no-scrollbar">
                 {conversionLog.map((log) => (
                     <div
                         key={log.id}
-                        className="flex justify-between items-center h-[64px] gap-200 p-200 bg-neutral-600/80 rounded-lg border border-neutral-600/50 hover:border-neutral-500/50 transition-all duration-150"
+                        className="flex justify-between items-center h-[64px] gap-150 p-150 md:gap-200 md:p-200 bg-neutral-600/80 rounded-lg border border-neutral-600/50 hover:border-neutral-500/50 transition-all duration-150"
                     >
-                        <div className="flex items-center">   
+                        <div className="flex flex-col gap-050 md:flex-row items-center">
                             {/* Tempo transcorrido relativo */}
-                            <div className="text-preset-4 text-neutral-200 gap-100 bg-neutral-900/60 w-[64px] rounded-full border border-neutral-700/30">
+                            <div className="text-preset-4 self-start text-neutral-200 gap-100 bg-neutral-900/60 w-[64px] rounded-full border border-neutral-700/30">
                                 {formatTimeAgo(log.timestamp)}
                             </div>
                             <span className="text-neutral-50 text-preset-4">{log.fromCurrency.code} ➔ {log.toCurrency.code}</span>
                         </div>
-                        <div className="flex items-center gap-4">
+                        <div className="flex gap-125 items-center">
                             {/* Cantidades e Moedas de orixe e destino */}
-                            <span className="text-neutral-100 text-preset-3">
-                                {log.fromAmount}
-                            </span>
-                            <span className="text-lime-500 text-preset-3">
-                                {Number(log.toAmount).toFixed(2)}
-                            </span>
-                            <img onClick={() => deleteLogEntry(log.id)} src="./images/icon-delete.svg" alt="delete"/>
+                            <div className="flex flex-col md:flex-row md:gap-250 gap-025 items-end">
+                                <span className="text-neutral-100 text-preset-3">
+                                    {log.fromAmount !== undefined && log.fromAmount !== null
+                                        ? Number(log.fromAmount).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+                                        : "0.00"
+                                    }
+                                </span>
+                                <span className="text-lime-500 text-preset-3">
+                                    {log.toAmount !== undefined && log.toAmount !== null
+                                        ? Number(log.toAmount).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+                                        : "0.00"
+                                    }
+                                </span>
+                            </div>
+                            <img onClick={() => deleteLogEntry(log.id)} src="./images/icon-delete.svg" alt="delete" />
                         </div>
                     </div>
                 ))}
