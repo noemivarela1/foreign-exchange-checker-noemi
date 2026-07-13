94b6fe7d61373620f1e5411a2dd2dcdae843195e
Noemi <noemivarela1@gmail.com>
2026-07-13 08:09:17 +0200

    Another small change in dimensions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import LiveMarketsTicker from './LiveMarketsTicker'
 export default function Header({ totalMoedas }) {
     return (
         <header className="main-header flex flex-col">
-            <div className="flex items-center justify-between  md:px-6 md:py-5 p-200">
+            <div className="flex items-center justify-between  md:px-300 md:py-250 h-[66px] p-200">
                 <a href="./" className="gap-2 text-lg font-semibold text-slate-800 w-[28.6vw] max-w-[139px] min-w-[107px]  h-auto">
                     <img src="./images/logo.svg" alt="Logo da empresa" className="logo h-8 w-auto" />
                 </a>
